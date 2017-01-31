03fc07a696fa22f0bb800dd74c73e7306f433198
LavanyaDNR <lavanya.dnr@gmail.com>
2017-01-30 18:14:56 -0600

    some changes

--- challenge/src/main/java/challenge/ElevatorOperation.java
+++ challenge/src/main/java/challenge/ElevatorOperation.java
@@ -2,23 +2,24 @@ package challenge;
 
 public class ElevatorOperation {
 	
-	int noOfElevators = 4;
-	int noOfFloors = 10;
 	
-	void outsideOperation(int currentFloor, String operation){
-		if(currentFloor == 1 && operation.equals("down")){
+	ElevatorController elevatorController = new ElevatorController();
+	
+	void elevatorOperation(int currentFloor, String direction){
+		if(currentFloor == 1 && direction.equals("down")){
 			return;
-		}else if(currentFloor == 10 && operation.equals("up")){
+		}else if(currentFloor == 10 && direction.equals("up")){
 			return;
 		}
 		else{
-			findAvailableElevator(currentFloor, operation);
+			Elevator elevator = elevatorController.availableElevator(currentFloor, direction);
+			//add wait till elevator reaches the destination
+			elevator.setFloorPosition(currentFloor);
+			elevator.setClosed(false);
 		}
 	}
-	void findAvailableElevator(int currentFloor, String operation){
-		
-	}
-
+	
+	
 
 
 }
